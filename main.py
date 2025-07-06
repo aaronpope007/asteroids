63083cf3dd17a61136d3ef1927fcd44a750e45f5
Aaron Pope <aaronpope007@gmail.com>
2025-07-06 10:00:19 -0500

    pygame window opening

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-        screen.fill((0,0,0), rect=None)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
         pygame.display.flip()
+
 
 
 
